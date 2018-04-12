9305010d46fb9e83b16ed62812d81b3e584a2321
KasenLi <kasenkelvin.10@gmail.com>
2018-04-12 15:31:56 -0400

    images path

--- resources/views/admin/images/index.blade.php
+++ resources/views/admin/images/index.blade.php
@@ -7,7 +7,7 @@
 		@foreach($images as $image)
 			<div class="col-md-4">
 				<div class="card" style="width: 18rem;">
-  					<img class="card-img-top" src="admin/images/articles/{{ $image->name}}" alt="Card image cap">
+  					<img class="card-img-top" src="/images/articles/{{ $image->name}}" alt="Card image cap">
   					<div class="card-body">
     					<p class="card-text">{{$image->article->title}}</p>
   					</div>
