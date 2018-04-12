e8d35b96de10bc412cd78e0a5121d6165cdd316d
KasenLi <kasenkelvin.10@gmail.com>
2018-04-12 17:30:11 -0400

    Cors

--- resources/views/front/template/main.blade.php
+++ resources/views/front/template/main.blade.php
@@ -1,11 +1,8 @@
-<?php
- header("Access-Control-Allow-Origin: *"); 
- ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+	<!--<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">-->
 	<title>@yield('title','Home') | Blog K</title>
 	<link rel="stylesheet" href="{{ asset('plugins/bootstrap/css/sandstone/bootstrap.css')}}">
 	<link rel="stylesheet" href="{{ asset('css/general.css')}}">
